00d75e77d2405cfad9c02689c8b01239a9554f6d
londaka <sanjayapokhrel41@gmail.com>
2024-11-07 09:31:13 +0545

    Add script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+let qrcode = new QRCode(document.querySelector(".qrcode"));
+
+qrcode.makeCode("Why did you scan me?");
+
+function generatorQr(){
+    if(document.querySelector("input").value === "" || document.querySelector("input").value === ""){
+      alert("Input Field Can not be blank!")
+    }else{
+        qrcode.makeCode(document.querySelector("input").value);
+    }
+}
